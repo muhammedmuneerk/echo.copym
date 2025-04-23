a3e46305ae5a17d0110923da3752174cbe5b4e62
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2025-04-23 17:00:27 +0530

    comment/remove SpinningBlockchain 3D model from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function HomePage() {
       <GreenTokenizationPopup />
       <GoldTokenizationPopup/>
       <div style={{ position: 'relative' }}>
-        <SpinningBlockchain />
+        {/* <SpinningBlockchain /> */}
         <Blockchains />
         <Features />
         <GlobalMarkets />
